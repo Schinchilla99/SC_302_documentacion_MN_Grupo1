f74e51b9415da0bb5f7b9ccdce088f9881ae9179
Schinchilla99 <157663258+Schinchilla99@users.noreply.github.com>
2024-02-05 08:29:14 -0600

    Update mi-archivo.js

--- mi-archivo.js
+++ mi-archivo.js
@@ -1,5 +1,33 @@
 alert("hola mundo"); 
 alert("hola mundo_editado"); 
 Console.log(“cambios en el archivo”);
-bower.json
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>jQuery  Datepicker </title>
+		  <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+		  <link rel="stylesheet" href="style.css">
+		  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+		  <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+		  <script src="jquery.ui.datepicker-es.js"></script>
+		<script>
 
+		   $(function() {
+
+		    	$( "#datepicker" ).datepicker({
+						regional: "Es",
+					  changeMonth: true,
+					  changeYear: true,
+					  yearRange: '1925:2025'
+					});
+
+		   });
+
+		</script>
+	</head>
+	 <body>
+	    <p>Date: <input type="text" id="datepicker">
+	 </body>
+</html>
